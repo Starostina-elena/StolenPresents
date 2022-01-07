3039a9a5c5e10d2626c27f2e40a20b7a36f7b7a6
ShapochkinaAlisa <94118995+ShapochkinaAlisa@users.noreply.github.com>
2022-01-07 20:09:57 +0400

    Update stroyka.py

--- stroyka.py
+++ stroyka.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import os
+import pygame_gui
 
 pygame.init()
 size = width, height = 500, 500
@@ -8,6 +9,8 @@ screen = pygame.display.set_mode(size)
 horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
+result = 0
+
 
 
 def load_image(name, colorkey=None):
@@ -42,7 +45,7 @@ count = 0
 class Landing(pygame.sprite.Sprite):
     image = load_image("kvaddd.png")
     def __init__(self, pos):
-        global my, y_pos, count
+        global my, y_pos, count, result
         super().__init__(all_sprites)
         self.image = Landing.image
         self.rect = self.image.get_rect()
@@ -50,34 +53,48 @@ class Landing(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.x = pos[0]
         self.rect.y = pos[1]
-        res = 0
-        my += [self.rect.x, self.rect.y]
-        print(my)
-        count += 1
-        if count != 1:
-            y_pos -= 25
+        print(count, result, my, 1)
 
     def update(self):
-        global y_pos, count, res
-        if not pygame.sprite.collide_mask(self, mountain) and self.rect.y < y_pos:
-            self.rect = self.rect.move(0, 1)
-        if count != 1:
-            if abs(int(my[-2]) - int(my[0])) > 12:
-                print("Game over")
-                exit()
-
+        try:
+            global y_pos, count, result, my
+            if event.type == pygame.MOUSEBUTTONDOWN and count != 1:
+                my += [self.rect.x, self.rect.y]
+                count += 1
+                if count != 1:
+                    y_pos -= 25
+            print(count, result, my, 2)
+            if result % 2 != 0:
+                self.rect.x += 5
+                if self.rect.left > width:
+                    self.rect.right = 0
+            else:
+                if not pygame.sprite.collide_mask(self, mountain) and self.rect.y < y_pos:
+                    self.rect = self.rect.move(0, 1)
+                else:
+                    Landing([122, 20])
+                if count != 0:
+                    if abs(int(my[-2]) - int(my[0])) > 12:
+                        print("Game over")
+                        exit()
+        except Exception as a:
+            print(a)
 
 
 clock = pygame.time.Clock()
 running = True
 while running:
     screen.fill((255, 255, 255))
-    t = clock.tick(45)
+    t = clock.tick(20)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            Landing(event.pos)
+            result += 1
+            if result == 1:
+                Landing(event.pos)
+            else:
+                all_sprites.update()
     all_sprites.draw(screen)
     all_sprites.update()
     pygame.display.flip()
