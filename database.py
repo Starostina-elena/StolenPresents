74b8a8f4c6e14c310faa599eaf0598b4942945f1
Julia <94120381+Rilyneet@users.noreply.github.com>
2022-01-23 16:32:25 +0400

    Add files via upload
    доработала вывод результатов

--- database.py
+++ database.py
@@ -25,8 +25,26 @@ def results():
     return result.fetchall()
 
 
+def load_image(name, colorkey=0, transform=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    if transform:
+        image = pygame.transform.scale(image, (40, 40))
+    return image
+
+
 def show_highscores():
-    # вывод результатов игры
+    # вывод результатов
     res = results()
     background_colour = (255, 255, 255)
     (width, height) = (550, 550)
@@ -39,19 +57,32 @@ def show_highscores():
     start_y = 100
     res.insert(0, ['Игрок', 'Подарков', 'Время'])
     for i, line in enumerate(res[:10]):
-        user = " ".join([str(a).strip().ljust(18, ' ') for a in line])
-        print(user)
-        string_rendered = font.render(user, 14, pygame.Color('white'))
-        hs_rect = string_rendered.get_rect()
-        hs_rect.y = i * 30 + start_y
-        hs_rect.x = 50
+        user = line[0]
+        gifts = line[1]
+        time = line[2]
+        print(user, gifts, time)
 
-        screen2.blit(string_rendered, hs_rect)
+        user_rendered = font.render(user, 14, pygame.Color('white'))
+        user_rect = user_rendered.get_rect()
+        user_rect.y = i * 30 + start_y
+        user_rect.x = 20
+
+        gifts_rendered = font.render(str(gifts), 14, pygame.Color('white'))
+        gifts_rect = gifts_rendered.get_rect()
+        gifts_rect.y = i * 30 + start_y
+        gifts_rect.x = 270
+
+        time_rendered = font.render(str(time), 14, pygame.Color('white'))
+        time_rect = time_rendered.get_rect()
+        time_rect.y = i * 30 + start_y
+        time_rect.x = 400
+
+        screen2.blit(user_rendered, user_rect)
+        screen2.blit(gifts_rendered, gifts_rect)
+        screen2.blit(time_rendered, time_rect)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 return
-            elif event.type == pygame.QUIT:
-                sys.exit()
         pygame.display.flip()
         pygame.display.update()
